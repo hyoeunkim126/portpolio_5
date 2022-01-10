e274065ccce11fc8684565fed805441bc191402f
hyoeunkim126 <hyoeun126@naver.com>
2022-01-10 18:00:30 +0900

    aa

--- js/evaarmisen.js
+++ js/evaarmisen.js
@@ -24,11 +24,28 @@ $(function(){
       } else {
           $('header').css('background-color', 'rgb(250, 247, 240)'); //ivory색상
       }
+
       var s1=$('.about-me').offset().top;
       if(ws < s1-200){
         $('.about-me').css('background-color', 'rgb(240, 219, 213)'); //pink색상
       }else {
         $('.about-me').css('background-color', 'rgb(250, 247, 240)'); //ivory색상
-    }
+      }
+
+      var s2=$('.work').offset().top;
+      if(ws < s2-200){
+        $('.work').css('background-color', 'rgb(240, 219, 213)'); //pink색상
+      }else {
+        $('.work').css('background-color', 'rgb(250, 247, 240)'); //ivory색상
+        $('.about-me').css('background-color', 'rgb(240, 219, 213)'); //pink색상
+      }
+
+      var s3=$('.collaborations').offset().top;
+      if(ws < s3-200){
+        $('.collaborations').css('background-color', 'rgb(240, 219, 213)'); //pink색상
+      }else {
+        $('.collaborations').css('background-color', 'rgb(250, 247, 240)'); //ivory색상
+        $('.work').css('background-color', 'rgb(240, 219, 213)'); //pink색상
+      }
   });
 });
